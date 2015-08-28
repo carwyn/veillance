766727eab8780b80339f13e814a1353e1cfc0605
Carwyn Edwards <carwyn@carwyn.com>
2015-08-28 04:42:02 +0100

    Remove add branches button.

--- veilweb/client/veilweb.js
+++ veilweb/client/veilweb.js
@@ -33,22 +33,4 @@ Fragments.find().observe({
     }
 });
 
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-
-    // Add more branches.
-    addBranchFromStringArray(debugstringarray);
-  }
-});
 
