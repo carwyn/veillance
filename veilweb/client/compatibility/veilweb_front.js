076311fc92b930573ff048edea072f5954da04dc
Michael Flueckiger <contact@michaelflueckiger.ch>
2015-09-23 11:11:35 +0200

    Change Orbitcenter Button

--- veilweb/client/compatibility/veilweb_front.js
+++ veilweb/client/compatibility/veilweb_front.js
@@ -1,8 +1,7 @@
 
-// debug adding branches
-document.addEventListener( 'dblclick', onDoubleckilck, false );
 document.addEventListener( 'dblclick', onDoubleckilck, false );
 window.addEventListener("keydown", onkeypress, false);
+
 
 // debug string var
 var debugstring = "TRELAWNEY Dr Livesey, and the rest of these gentlemen, having asked me to write down the whole particulars about Treasure Island, from the beginning to the end, keeping nothing back but the bearings of the island, and that only because there, is still treasure not yet lifted, I take up my pen in the year of, grace 17__ and go back to the time when my father kept the Admiral Benbow inn, and the brown old seaman with the sabre cut first took up his lodging under our roof, I remember him as if it were yesterday, as he came plodding to the inn door, his sea-chest following behind him in a hand-barrow—a tall, strong, heavy, nut-brown man, his tarry pigtail falling over the shoulder, of his soiled blue coat, his hands ragged and scarred, with black, broken nails, and the sabre cut across one cheek, a dirty, livid white. I remember him looking round the cover and whistling to himself as he did so, and then breaking out in that old sea-song that he sang so often afterwards";
@@ -352,5 +351,11 @@ function handleClick(myRadio) {
     console.log(myRadio.value);
 }
 
+function changeOrbitCenter(){
+  
+       var p = Math.floor(Math.random() * positions.length);
+       var pos = positions[p];
+       controls.target.set(pos.x,pos.y,pos.z);
 
+}
 
