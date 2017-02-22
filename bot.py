e15d6f4470820c661f64bdea17544c404c80d15b
Carwyn Edwards <carwyn.edwards@bangor.ac.uk>
2017-02-22 12:02:42 +0000

    Add abaility to set bot source IP address.

--- bot.py
+++ bot.py
@@ -1,11 +1,32 @@
-import urllib2
 import time
+import functools
+import httplib
+import urllib2
 
+#BOT_SITE = "http://news.bbc.co.uk/"
 BOT_SITE = "http://news.bbc.co.uk/"
+
+#SOURCE_IP = "10.42.0.104"
+SOURCE_IP = "10.42.0.188"
 
 SLEEP_TIME = 60
 
+class BoundHTTPHandler(urllib2.HTTPHandler):
+
+    def __init__(self, source_address=None, debuglevel=0):
+        urllib2.HTTPHandler.__init__(self, debuglevel)
+        self.http_class = functools.partial(httplib.HTTPConnection,
+                source_address=source_address)
+
+    def http_open(self, req):
+        return self.do_open(self.http_class, req)
+
+
 def main():
+
+    handler = BoundHTTPHandler(source_address=(SOURCE_IP, 0))
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
     
     while True:
         f = urllib2.urlopen(BOT_SITE)
