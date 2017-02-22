29b88d25d08c5bf946cddbfd04f872f30ef18e3e
Carwyn Edwards <carwyn.edwards@bangor.ac.uk>
2017-02-22 16:51:24 +0000

    Parameterise bot.

--- bot.py
+++ bot.py
@@ -2,14 +2,10 @@ import time
 import functools
 import httplib
 import urllib2
+import argparse
 
-#BOT_SITE = "http://news.bbc.co.uk/"
-BOT_SITE = "http://news.bbc.co.uk/"
-
-#SOURCE_IP = "10.42.0.104"
-SOURCE_IP = "10.42.0.188"
-
-SLEEP_TIME = 60
+# GREEN = "10.42.0.104"
+# PURPLE = "10.42.0.188"
 
 class BoundHTTPHandler(urllib2.HTTPHandler):
 
@@ -21,20 +17,28 @@ class BoundHTTPHandler(urllib2.HTTPHandler):
     def http_open(self, req):
         return self.do_open(self.http_class, req)
 
-
 def main():
 
-    handler = BoundHTTPHandler(source_address=(SOURCE_IP, 0))
-    opener = urllib2.build_opener(handler)
-    urllib2.install_opener(opener)
+    parser = argparse.ArgumentParser(description="Veillance Bot")
+    parser.add_argument("--site", default="http://news.bbc.co.uk/")
+    parser.add_argument("--source")
+    parser.add_argument("--sleep", default=60, type=int)
+
+    args = parser.parse_args()
+
+    if args.source:
+        print "Using Source IP:", args.source
+        handler = BoundHTTPHandler(source_address=(args.source, 0))
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
     
     while True:
-        f = urllib2.urlopen(BOT_SITE)
+        f = urllib2.urlopen(args.site)
         content = f.readlines()
 
         print f.getcode(), f.geturl(), "Length: ", len(content)
     
-        time.sleep(SLEEP_TIME)
+        time.sleep(args.sleep)
 
 if __name__ == "__main__": main()
 
